d7ecc8949b57425605fa3a617e3899aa88148b4e
kjjkjjzyayufqza <47631064+kjjkjjzyayufqza@users.noreply.github.com>
2025-12-28 14:30:04 +0800

    chore: app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import "./App.css";
 import SidebarLayout from "./layout/Sidebar";
-import { Route, RouterProvider, Routes } from "react-router";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { HashRouter } from "react-router-dom";
 import { RouterItems } from "./router/router";
 import { useConfigStore } from "./store/configStore";
 import { useEffect } from "react";
+
 function App() {
   const { initStore } = useConfigStore();
   useEffect(() => {
     initStore()
   }, [])
+  
   return (
     <HashRouter>
-      <SidebarLayout>
-        <Routes>
+      <Routes>
+        {/* Layout Route - all routes with sidebar */}
+        <Route element={<SidebarLayout />}>
           {RouterItems.map((e, i) => {
             return <Route key={i} path={e.url} element={e.element} />;
           })}
-        </Routes>
-      </SidebarLayout>
+        </Route>
+        
+        {/* Future: Routes without sidebar can be added here */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
     </HashRouter>
   );
 }
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,21 +1,22 @@
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { Toaster } from "../components/ui/sonner"
 import { TopNavBar } from "@/components/TopNavBar"
+import { Outlet } from "react-router"
 
-export default function SidebarLayout({ children }: { children: React.ReactNode }) {
+export default function SidebarLayout() {
   return (
-    <>
+    <div className="flex h-svh flex-col">
       <TopNavBar />
-      <SidebarProvider>
+      <SidebarProvider className="flex flex-1 min-h-0">
         <AppSidebar />
-        <SidebarInset className="flex min-w-0 flex-col h-screen overflow-hidden">
-          <div className="min-w-0 p-6">
-            {children}
+        <main className="flex flex-1 flex-col bg-background overflow-hidden">
+          <div className="min-w-0 p-6 flex-1 overflow-auto">
+            <Outlet />
           </div>
-        </SidebarInset>
+        </main>
         <Toaster />
       </SidebarProvider>
-    </>
+    </div>
   )
 }
